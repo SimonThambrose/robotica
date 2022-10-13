cba570dab3ef9fc8c6f16a0312bd1eb1a3072f99
KaiVrijenhoeff <Kaiv2000@hotmail.com>
2022-10-13 11:57:56 +0200

    Removed some comments

--- code/code.ino
+++ code/code.ino
@@ -72,11 +72,6 @@ void loop() {
     right();
   }
 
-//   //Left or right
-//   else if (!sens[0] && !sens[1] && !sens[2] && !sens[3] && !sens[4]) {
-//     leftRight();
-//   }
-
 //   //Straight or left
 //   else if (!sens[0] && !sens[1] && !sens[2] && sens[3] && sens[4]) {
 //     straightLeft();
@@ -176,15 +171,6 @@ void right() {
   analogWrite(leftSpeed, 0);
   delay(200);
 }
-
-// //Left or right
-// void leftRight() {
-//   digitalWrite(leftDirection, LOW);
-//   analogWrite(rightSpeed, 20);
-//   digitalWrite(rightDirection, LOW);
-//   analogWrite(leftSpeed, 20);
-//   delay(500);
-// }
 
 // //Straight or left
 // void straightLeft() {
